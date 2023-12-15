f9b0fada603179522eb4502f65609958b20ce776
sarahdurant <sarah.ash.durant@gmail.com>
2023-12-15 14:03:37 -0700

    separate editor functionality into its own file

--- src/editor.rs
+++ src/editor.rs
@@ -1,30 +1,29 @@
 use std::{
-  io::{self},
   time::{Duration},
+  error::Error,
 };
 use crossterm::{
   event::{poll, read, Event, KeyModifiers, KeyCode},
   terminal::{disable_raw_mode, enable_raw_mode},
 };
 
-pub struct Editor {}
+pub struct Editor {
+  should_quit: bool,
+}
 
 impl Editor {
-  pub fn run(&self) -> io::Result<()> {
+  pub fn run(&mut self) -> Result<(), Box<dyn Error>> {
     enable_raw_mode()?;
 
     loop {
         if poll(Duration::from_millis(500))?{
-            if let Event::Key(event) = read()? {
-                let key = event.code;
-                let modifiers = event.modifiers;
-                
-                println!("{:?}\r", key);         
-                println!("{:?}\r", modifiers);         
-                if key == KeyCode::Char('q') && modifiers == KeyModifiers::CONTROL {
-                    println!("Exiting... Good-bye!");
-                    break;
-                }
+            if let Err(error) = self.process_keypress() {
+              disable_raw_mode()?;
+              panic!("{error}");
+            }
+
+            if self.should_quit {
+              break;
             }
         }
     }
@@ -32,8 +31,26 @@ impl Editor {
     Ok(())
   }
 
+  pub fn process_keypress(&mut self) -> Result<(), Box<dyn Error>> {
+      match read() {
+        Ok(Event::Key(event)) => {
+              let key = event.code;
+              let modifiers = event.modifiers;
+      
+              println!("{key:?}\r");         
+              println!("{modifiers:?}\r");         
+              if key == KeyCode::Char('q') && modifiers == KeyModifiers::CONTROL {
+                  self.should_quit = true;
+              }
+              Ok(())
+          },
+        Err(_) => Err("Error in read!".into()),
+        _ => Ok(()),
+    }
+  }
+
   pub fn default() -> Self {
-    Editor{}
+    Editor{ should_quit: false}
   }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,6 @@ mod editor;
 
 use editor::Editor;
 
-// fn fatal_err(e: std::io::Error) {
-//     panic!("{}", e);
-// }
 
 // fn to_ctrl_byte(c: char) -> u8 {
 //     let byte = c as u8;
@@ -12,6 +9,5 @@ use editor::Editor;
 // }
 
 fn main()  {
-    let editor = Editor::default();
-    let _ = editor.run();
+    let _ = Editor::default().run();
 }

345d1d690cfa467db994d5906fed8ac5e2ddfe37
sarahdurant <sarah.ash.durant@gmail.com>
2023-12-21 14:42:39 -0700

    move stdout to inside terminal

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,10 +1,23 @@
+use std::{
+    io::{stdout, Write},
+    time::{Duration},
+    error::Error,
+};
+use crossterm::{
+    QueueableCommand, cursor,
+    event::{poll, read, Event, KeyModifiers, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode, ClearType},
+};
+
+
 pub struct Size {
     pub width: u16,
     pub height: u16,
 }
 
 pub struct Terminal {
-    size: Size
+    size: Size,
+    stdout: std::io::Stdout,
 }
 
 impl Terminal {
@@ -19,6 +32,7 @@ impl Terminal {
                 width: cols,
                 height: rows,
             },
+            stdout: stdout(),
         })
     }
 
@@ -27,9 +41,23 @@ impl Terminal {
         &self.size
     }
 
-    pub fn draw_left_margin(&self, margin: &str) {
+    pub fn draw_left_margin(&mut self, margin: &str) -> Result<(), std::io::Error> {
+        //stdout.queue(cursor::SavePosition());
         for _ in 0..self.size.height {
             println!("{margin}\r");
         }
+        //stdout.queue(cursor::RestorePosition());
+        self.stdout.flush()?;
+        //println!("{:?}", orig_cursor);
+
+        Ok(())
+    }
+
+    pub fn initialize_screen(&mut self) -> Result<(), std::io::Error> {
+        self.stdout.queue(crossterm::terminal::Clear(ClearType::All))?;
+        self.stdout.queue(cursor::MoveTo(0,0))?;
+        self.stdout.flush()?;
+
+        Ok(())
     }
 }
